f71ed408d4e74fd8093588f75df078083e1b5eb9
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-04-17 23:17:10 +0700

    changes

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import ru_local as ru
 rsd = input(ru.RESIDENT)
-if rsd in ['да', 'ДА', 'Да', 'дА']:
-    a = input("res_q_1")
-    if a.lower()=="да":
-        print("answer", "9%")
+if rsd.lower() == 'да' or rsd.lower() == 'yes':
+    a = input(ru.RES_Q_1)
+    if a.lower() == 'да' or a.lower() == 'yes':
+        print(ru.ANSWER, "9%")
     else:
-        b = input("res_q_2")
-        if b.lower()=="да":
-            print("answer", "35%")
+        b = input(ru.RES_Q_2)
+        if b.lower() == 'да' or b.lower() == 'yes':
+            print(ru.ANSWER, "35%")
         else:
-            c = input("res_q_3")
-            if c.lower()=="да":
-                print("answer", "35%")
+            c = input(ru.RES_Q_3)
+            if c.lower() == 'да' or c.lower() == 'yes':
+                print(ru.ANSWER, "35%")
             else:
-                d = input("res_q_4")
-                if d.lower()=="да":
-                    print("answer", "35%")
+                d = input(ru.RES_Q_4)
+                if d.lower() == 'да' or d.lower() == 'yes':
+                    print(ru.ANSWER, "35%")
                 else:
-                    e = input("res_q_5")
-                    if e.lower()=="да":
-                        print("answer", "13%")
+                    e = input(ru.RES_Q_5)
+                    if e.lower() == 'да' or e.lower() == 'yes':
+                        print(ru.ANSWER, "13%")
                     else:
-                        f = input("res_q_6")
-                        if f.lower()=="да":
-                            print("answer", "15% + 650 000", "currency")
+                        f = input(ru.RES_Q_6)
+                        if f.lower() == 'да' or f.lower() == 'yes':
+                            print(ru.ANSWER, "15% + 650 000", ru.CURRENCY)
                         else:
-                            print("answer", "13%")
+                            print(ru.ANSWER, "13%")
