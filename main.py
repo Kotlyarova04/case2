f64d74201a1fa2f65f6169200052316f0e767b2b
vitarafavich <v.rafaevich@g.nsu.ru>
2023-04-17 20:43:29 +0700

    -

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import ru_local as ru
 rsd = input(ru.RESIDENT)
 if rsd in ['да', 'ДА', 'Да', 'дА']:
+    a = input("res_q_1")
+    if a.lower()=="да":
+        print("answer", "9%")
+    else:
+        b = input("res_q_2")
+        if b.lower()=="да":
+            print("answer", "35%")
+        else:
+            c = input("res_q_3")
+            if c.lower()=="да":
+                print("answer", "35%")
+            else:
+                d = input("res_q_4")
+                if d.lower()=="да":
+                    print("answer", "35%")
+                else:
+                    e = input("res_q_5")
+                    if e.lower()=="да":
+                        print("answer", "13%")
+                    else:
+                        f = input("res_q_6")
+                        if f.lower()=="да":
+                            print("answer", "15% + 650 000", "currency")
+                        else:
+                            print("answer", "13%")
