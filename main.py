0d35ad6315bb92b7546639e2aa4364b9d91c55b2
vitarafavich <v.rafaevich@g.nsu.ru>
2023-04-19 17:34:04 +0700

    -

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import ru_local as ru
-prd = input(ru.PERIOD)
+import datetime
+from datetime import date
+
+d = datetime.datetime.today()
+do = d.date()
+d_before = (do - datetime.timedelta(days=365))
+print(ru.PERIOD, d_before, ru.R_2, do, ru.PERIOD_)
+prd = input()
 if prd.lower() == 'да' or prd.lower() == 'yes':
     print(ru.RESIDENT)
     a = input(ru.RES_Q_1)
@@ -68,5 +75,53 @@ else:
                         else:
                             print(ru.ANSWER, '30%')
     else:
-        print(ru.DATES)
+        d = datetime.datetime.today()
+        do = d.date()
+        d_before = (do - datetime.timedelta(days=365))
+        e = input()
+        if e.lower() == ru.R_4:
+            f = int(input(ru.R_5))
+            total = 0
+            for i in range(f):
+                a = input(ru.R_6)
+            b = input(ru.R_7)
+            a = a.split('-')
+            b = b.split('-')
+            aa = datetime.date(int(a[0]), int(a[1]), int(a[2]))
+            bb = datetime.date(int(b[0]), int(b[1]), int(b[2]))
+            cc = bb - aa
+            n = str(cc.days)
+            total += int(n)
+            print(total)
+            if total <= 183:
+                print(ru.R_8)
+                a = input(ru.RES_Q_1)
+                if a.lower() == 'да' or a.lower() == 'yes':
+                    print(ru.ANSWER, "9%")
+                else:
+                    b = input(ru.RES_Q_2)
+                    if b.lower() == 'да' or b.lower() == 'yes':
+                        print(ru.ANSWER, "35%")
+                    else:
+                        c = input(ru.RES_Q_3)
+                        if c.lower() == 'да' or c.lower() == 'yes':
+                            print(ru.ANSWER, "35%")
+                        else:
+                            d = input(ru.RES_Q_4)
+                            if d.lower() == 'да' or d.lower() == 'yes':
+                                print(ru.ANSWER, "35%")
+                            else:
+                                e = input(ru.RES_Q_5)
+                                if e.lower() == 'да' or e.lower() == 'yes':
+                                    print(ru.ANSWER, "13%")
+                                else:
+                                    f = input(ru.RES_Q_6)
+                                    if f.lower() == 'да' or f.lower() == 'yes':
+                                        print(ru.ANSWER, "15% + 650 000", ru.CURRENCY)
+                                    else:
+                                        print(ru.ANSWER, "13%")
+            else:
+                print(ru.R_9)
+
+
 
